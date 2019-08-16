4d3b3e1d06388d7e0a021bcc576b73a6b745713e
MohamedChahik <simohamed.chahik@gmail.com>
2019-08-16 23:08:35 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTGujo8WPbymhzstSQJHLc0ItWZ8XFr1o",
+   authDomain: "notifs-249901.firebaseapp.com",
+   databaseURL: "https://notifs-249901.firebaseio.com",
+   projectId: "notifs-249901",
+   storageBucket: "",
+   messagingSenderId: "220814626486",
+};
